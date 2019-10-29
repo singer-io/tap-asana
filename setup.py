fa506db61a1d5a60e246304cb36616adfe509c29
Dan Mosora <dan@stitchdata.com>
2019-10-29 21:25:16 +0000

    Version 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-asana",
-    version="0.0.1",
+    version="0.0.2",
     description="Singer.io tap for extracting Asana data",
     author="Stitch",
     url="http://github.com/singer-io/tap-asana",
