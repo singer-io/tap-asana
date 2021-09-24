1a5343eef2721326a2415dfbab4fbd0293def8d3
harshpatel4_crest <harsh.patel4@crestdatasys.com>
2021-09-24 13:39:42 +0530

    added caching for project ids

--- tap_asana/streams/stories.py
+++ tap_asana/streams/stories.py
@@ -59,14 +59,22 @@ class Stories(Stream):
     bookmark = self.get_bookmark()
     session_bookmark = bookmark
     opt_fields = ",".join(self.fields)
+
+    # list of project ids
+    project_ids = []
     for workspace in self.call_api("workspaces"):
       for project in self.call_api("projects", workspace=workspace["gid"]):
-        for task in self.call_api("tasks", project=project["gid"]): 
-          task_gid = task.get('gid')
-          for story in Context.asana.client.stories.get_stories_for_task(task_gid=task_gid, opt_fields=opt_fields):
-            session_bookmark = self.get_updated_session_bookmark(session_bookmark, story[self.replication_key])
-            if self.is_bookmark_old(story[self.replication_key]):
-              yield story
+        project_ids.append(project["gid"])
+
+    # iterate over all project ids and continue fetching
+    for project_id in project_ids:
+      for task in self.call_api("tasks", project=project_id):
+        task_gid = task.get('gid')
+        for story in Context.asana.client.stories.get_stories_for_task(task_gid=task_gid, opt_fields=opt_fields):
+          session_bookmark = self.get_updated_session_bookmark(session_bookmark, story[self.replication_key])
+          if self.is_bookmark_old(story[self.replication_key]):
+            yield story
+
     self.update_bookmark(session_bookmark)
 
 
--- tap_asana/streams/tasks.py
+++ tap_asana/streams/tasks.py
@@ -53,12 +53,20 @@ class Tasks(Stream):
     session_bookmark = bookmark
     modified_since = bookmark.strftime("%Y-%m-%dT%H:%M:%S.%f")
     opt_fields = ",".join(self.fields)
+
+    # list of project ids
+    project_ids = []
     for workspace in self.call_api("workspaces"):
       for project in self.call_api("projects", workspace=workspace["gid"]):
-        for task in self.call_api("tasks", project=project["gid"], opt_fields=opt_fields, modified_since=modified_since): 
-          session_bookmark = self.get_updated_session_bookmark(session_bookmark, task[self.replication_key])
-          if self.is_bookmark_old(task[self.replication_key]):
-            yield task
+        project_ids.append(project["gid"])
+
+    # iterate over all project ids and continue fetching
+    for project_id in project_ids:
+      for task in self.call_api("tasks", project=project_id, opt_fields=opt_fields, modified_since=modified_since): 
+        session_bookmark = self.get_updated_session_bookmark(session_bookmark, task[self.replication_key])
+        if self.is_bookmark_old(task[self.replication_key]):
+          yield task
+
     self.update_bookmark(session_bookmark)
 
 
