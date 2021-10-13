4ea758de343cf4fc74e4212401cb645b2494b012
harshpatel4_crest <harsh.patel4@crestdatasys.com>
2021-10-13 18:13:40 +0530

    updated the unittest case

--- tests/unittests/test_maximum_page_size.py
+++ tests/unittests/test_maximum_page_size.py
@@ -3,11 +3,11 @@ from unittest import mock
 from tap_asana.streams.base import Stream
 from tap_asana import context
 
-@mock.patch("tap_asana.streams.base.LOGGER.warn")
+@mock.patch("tap_asana.streams.base.LOGGER.warning")
 class TestMaximunPageSize(unittest.TestCase):
 
     # test case to verify that if page size greater 100 is passed in config then the tap gives warning
-    def test_page_size_greater_than_100(self, mocked_logger_warn):
+    def test_page_size_greater_than_100(self, mocked_logger_warning):
         # setting "results_per_page" in config file
         context.Context.config = {
             "client_id": "test_client_id",
@@ -20,11 +20,11 @@ class TestMaximunPageSize(unittest.TestCase):
         Stream()
 
         # verify if the LOGGER.warn was called and verify the message, as the "results_per_page" is greater than 100
-        self.assertEquals(mocked_logger_warn.call_count, 1)
-        mocked_logger_warn.assert_called_with("The page size cannot be greater than 100, hence setting to maximum page size: 100.")
+        self.assertEquals(mocked_logger_warning.call_count, 1)
+        mocked_logger_warning.assert_called_with("The page size cannot be greater than 100, hence setting to maximum page size: 100.")
 
     # test case to verify that there should be no warning is case of page size less than 100
-    def test_page_size_not_greater_than_100(self, mocked_logger_warn):
+    def test_page_size_not_greater_than_100(self, mocked_logger_warning):
         # setting "results_per_page" in config file
         context.Context.config = {
             "client_id": "test_client_id",
@@ -37,4 +37,4 @@ class TestMaximunPageSize(unittest.TestCase):
         Stream()
 
         # verify if the LOGGER.warn was not called, as the "results_per_page" is less than 100
-        self.assertEquals(mocked_logger_warn.call_count, 0)
+        self.assertEquals(mocked_logger_warning.call_count, 0)
