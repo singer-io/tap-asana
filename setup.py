25892bc025f5d8071f44e47a0ab102084ef8a2c4
Vivek Kumar <vivek.kumar25@globallogic.com>
2025-05-09 14:28:04 +0530

    Updated requirements

--- setup.py
+++ setup.py
@@ -10,17 +10,17 @@ setup(
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_asana"],
     install_requires=[
-        "asana==3.1.0",
-        'singer-python==5.13.0'
+        "asana==5.1.0",
+        "singer-python==6.1.1"
     ],
     extras_require={
-        'test': [
-            'pylint',
-            'requests==2.20.0',
+        "test": [
+            "pylint",
+            "requests==2.32.3",
             'nose'
         ],
-        'dev': [
-            'ipdb'
+        "dev": [
+            "ipdb"
         ]
     },
     entry_points="""
