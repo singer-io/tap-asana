1e29f252560356bbf5dee8a520671a65e9b11d83
Vivek Kumar <vivek.kumar25@globallogic.com>
2025-05-20 14:49:31 +0530

    Updated tags stream api call according to new sdk

--- tap_asana/streams/tags.py
+++ tap_asana/streams/tags.py
@@ -1,5 +1,6 @@
 from tap_asana.context import Context
 from tap_asana.streams.base import Stream
+import asana
 
 
 class Tags(Stream):
@@ -18,7 +19,7 @@ class Tags(Stream):
         "workspace",
     ]
 
-    def get_objects(self):
+    def z_get_objects(self):
         """Get stream object"""
         bookmark = self.get_bookmark()
         opt_fields = ",".join(self.fields)
@@ -32,6 +33,37 @@ class Tags(Stream):
                 )
                 if self.is_bookmark_old(tag[self.replication_key]):
                     yield tag
+        self.update_bookmark(session_bookmark)
+
+    def get_objects(self):
+        """Get stream object"""
+        bookmark = self.get_bookmark()
+        session_bookmark = bookmark
+        opt_fields = ",".join(self.fields)
+
+        # Use WorkspacesApi and TagsApi
+        workspaces_api = asana.WorkspacesApi(Context.asana.client)
+        tags_api = asana.TagsApi(Context.asana.client)
+
+        # Fetch workspaces using call_api
+        workspaces = self.call_api(workspaces_api, "get_workspaces")["data"]
+
+        # Iterate over all workspaces
+        for workspace in workspaces:
+            # Fetch tags for the current workspace
+            tag_response = self.call_api(
+                tags_api,
+                "get_tags",
+                opts={"workspace": workspace["gid"], "opt_fields": opt_fields},
+            )
+            for tag in tag_response["data"]:
+                session_bookmark = self.get_updated_session_bookmark(
+                    session_bookmark, tag[self.replication_key]
+                )
+                if self.is_bookmark_old(tag[self.replication_key]):
+                    yield tag
+
+        # Update the bookmark after processing all tags
         self.update_bookmark(session_bookmark)
 
 
