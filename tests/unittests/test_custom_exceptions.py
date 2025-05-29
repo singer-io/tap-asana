dc6eb315bbbb4bb11cc88fef90239b5bca7b0a34
Vivek Kumar <vivek.kumar25@globallogic.com>
2025-05-29 15:37:03 +0530

    removed unrequired tests

--- tests/unittests/test_custom_exceptions.py
+++ tests/unittests/test_custom_exceptions.py
@@ -11,26 +11,6 @@ from tap_asana.asana import Asana
 import asana
 
 
-class TestCustomExceptions(unittest.TestCase):
-    def test_invalid_token_error(self):
-        """Test InvalidTokenError exception."""
-        response_mock = {"message": "Invalid token"}
-        exception = InvalidTokenError(response=response_mock)
-
-        self.assertEqual(str(exception), "Invalid Token Error")
-        self.assertEqual(exception.status_code, 412)
-        self.assertEqual(exception.response, response_mock)
-
-    def test_no_authorization_error(self):
-        """Test NoAuthorizationError exception."""
-        response_mock = {"message": "Unauthorized access"}
-        exception = NoAuthorizationError(response=response_mock)
-
-        self.assertEqual(str(exception), "No Authorization Error")
-        self.assertEqual(exception.status_code, 401)
-        self.assertEqual(exception.response, response_mock)
-
-
 class TestStreamExceptions(unittest.TestCase):
     def setUp(self):
         """Set up the Asana client and mock context."""
