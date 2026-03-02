85ae1bcebcd5862e09e6dd6cbfab8c38e8ca28a5
mukeshbhatt18gl <mukesh.bhatt@qlik.com>
2026-03-02 10:40:12 +0530

    add comments for projects

--- tests/test_bookmarks.py
+++ tests/test_bookmarks.py
@@ -13,12 +13,9 @@ class AsanaBookmarksTest(AsanaBase):
 
     def test_run(self):
         # running sync with multiple date timestamps accross different streams due to differences in bookmark values
-        # Use fixed timestamps for projects so both syncs return records in test data.
-        projects_start_date_1 = "2026-02-25T00:00:00Z"
-        projects_start_date_2 = "2026-02-25T05:45:35Z"
-        # self.run_test(projects_start_date_1, projects_start_date_2, {"projects",})
         self.run_test("2023-11-28T00:00:00Z", "2023-11-30T00:00:00Z", {"subtasks",})
         # Removing Portfolios as they are only available for users in an Enterprise or Business plan.
+        # Removing Projects as data is available for the same date only.
         self.run_test("2019-01-28T00:00:00Z", "2023-11-30T00:00:00Z", self.expected_streams() - {"subtasks","projects","portfolios"})
 
     def parse_start_date_ts(self, start_date):
